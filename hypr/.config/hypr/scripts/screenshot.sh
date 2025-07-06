5ed80dd3f5de323f715bfaade78fa0e8b9a01efa
noxzym <orchitiadi@gmail.com>
2025-07-06 10:25:14 +0700

    update dots

--- hypr/.config/hypr/scripts/screenshot.sh
+++ hypr/.config/hypr/scripts/screenshot.sh
@@ -2,14 +2,15 @@
 
 choice=$(printf "Area\nWindow\nFullscreen" | tofi)
 sleep 0.3
+
 case "$choice" in
-    "Area")
-      grimblast --notify copy area
-      ;;
-    "Window")
-      grimblast --notify copy active
-      ;;
-    "Fullscreen")
-      grimblast --notify copy screen
-      ;;
+"Area")
+  grimblast --notify copy area
+  ;;
+"Window")
+  grimblast --notify copy active
+  ;;
+"Fullscreen")
+  grimblast --notify copy screen
+  ;;
 esac
