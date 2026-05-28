a225211e08a8c89633d8a2a5e704af9e797da0de
noxzym <orchitiadi@gmail.com>
2026-05-28 10:32:45 +0700

    chore: update power menu

--- ags/.config/ags/widget/Powermenu.tsx
+++ ags/.config/ags/widget/Powermenu.tsx
@@ -1,11 +1,12 @@
 import { execAsync } from "ags/process";
+import GLib from "gi://GLib";
 
 export default function Powermenu() {
 	return (
 		<button
 			onClicked={() => execAsync("/home/noxzym/.config/rofi/power.sh").catch(printerr)}
 		>
-			<label label="⏻" />
+			<label label={`󰣇 ${GLib.getenv("USER") ?? ""}`} />
 		</button>
 	);
 }
